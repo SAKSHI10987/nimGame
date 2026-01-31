2743ca198e18a4f0494eea9b5dc22af9d0e7ab32
SAKSHI10987 <ssakshi.g22@gmail.com>
2026-01-31 12:06:27 +0530

    adding one c++ file

---
+++ nimgame.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+//CHECKING IS THE CHANCES FOR WINNING THE FIRST PERSON PLAYING NIMGAME
+
+void nimGame(int n){
+    if(n%4==0){
+        cout<<"The first person will lose the game.";
+    }
+    else{
+        cout<<"The first person will win the game.";
+    }
+}
+
+
+
+int main(){
+    int n;
+    cout<<"enter the number:";
+    cin>>n;
+    nimGame(n);
+
+
+    return 0;
+
+}
